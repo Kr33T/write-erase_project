72b4f8495b20728804c12a5174c2323809837b8c
am321 <am321@DESKTOP-5NCTJOV>
2023-03-04 14:52:51 +0300

    Добавьте файлы проекта.

---
+++ write-erase_project/DBHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace write_erase_project
+{
+    internal class DBHelper
+    {
+        public static baseEntities bE;
+    }
+}
